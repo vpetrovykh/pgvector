b1b095d4a7ae7c816fb1eb711066d8188e348b4d
Andrew Kane <andrew@ankane.org>
2024-09-22 18:47:13 -0700

    Improved test [skip ci]

--- test/t/039_ivfflat_streaming.pl
+++ test/t/039_ivfflat_streaming.pl
@@ -14,7 +14,7 @@ $node->start;
 
 # Create table
 $node->safe_psql("postgres", "CREATE EXTENSION vector;");
-$node->safe_psql("postgres", "CREATE TABLE tst (i int4, v vector($dim));");
+$node->safe_psql("postgres", "CREATE TABLE tst (i int4 PRIMARY KEY, v vector($dim));");
 $node->safe_psql("postgres",
 	"INSERT INTO tst SELECT i, ARRAY[$array_sql] FROM generate_series(1, 100000) i;"
 );
@@ -28,13 +28,27 @@ my $count = $node->safe_psql("postgres", qq(
 ));
 is($count, 10);
 
-$count = $node->safe_psql("postgres", qq(
-	SET enable_seqscan = off;
-	SET ivfflat.probes = 10;
-	SET ivfflat.streaming = on;
-	SET ivfflat.max_probes = 50;
-	SELECT COUNT(*) FROM (SELECT v FROM tst WHERE i % 10000 = 0 ORDER BY v <-> (SELECT v FROM tst LIMIT 1) LIMIT 11) t;
-));
-isnt($count, 10);
+foreach ((30, 50, 70))
+{
+	my $max_probes = $_;
+	my $expected = $max_probes / 10;
+	my $sum = 0;
+
+	for my $i (1 .. 20)
+	{
+		$count = $node->safe_psql("postgres", qq(
+			SET enable_seqscan = off;
+			SET ivfflat.probes = 10;
+			SET ivfflat.streaming = on;
+			SET ivfflat.max_probes = $max_probes;
+			SELECT COUNT(*) FROM (SELECT v FROM tst WHERE i % 10000 = 0 ORDER BY v <-> (SELECT v FROM tst WHERE i = $i) LIMIT 11) t;
+		));
+		$sum += $count;
+	}
+
+	my $avg = $sum / 20;
+	cmp_ok($avg, '>', $expected - 2);
+	cmp_ok($avg, '<', $expected + 2);
+}
 
 done_testing();
